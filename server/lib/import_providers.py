df945ee88bf9f20eeb0101ab28a96c81c8093c2a
hategan <hategan@mcs.anl.gov>
2018-08-28 20:00:14 -0700

    Added classes to deal with resolving data IDs, lookups, etc.

---
+++ server/lib/import_providers.py
@@ -0,0 +1,38 @@
+from .entity import Entity
+from .data_map import DataMap
+from .file_map import FileMap
+
+
+class ImportProvider:
+    def __init__(self, name):
+        self.name = name
+
+    def getName(self) -> str:
+        return self.name
+
+    def matches(self, entity: Entity) -> bool:
+        raise NotImplementedError()
+
+    def lookup(self, entity: Entity) -> DataMap:
+        raise NotImplementedError()
+
+    def listFiles(self, entity: Entity) -> FileMap:
+        raise NotImplementedError()
+
+    def register(self, parent: object, parentType: str, progress, user, dataMap: DataMap,
+                 base_url: str=None):
+        raise NotImplementedError()
+
+
+class ImportProviders:
+    def __init__(self):
+        self.providers = []
+
+    def addProvider(self, provider: ImportProvider):
+        self.providers.append(provider)
+
+    def getProvider(self, entity: Entity) -> ImportProvider:
+        for provider in self.providers:
+            if provider.matches(entity):
+                return provider
+        raise Exception('Could not find suitable provider for entity %s' % entity)
