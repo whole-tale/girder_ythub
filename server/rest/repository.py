12f06a7b5f9c9dbe1410997675e74978f528e0a2
hategan <hategan@mcs.anl.gov>
2018-08-28 19:58:17 -0700

    Moved dataMap and fileMap into their own classes

--- server/rest/repository.py
+++ server/rest/repository.py
@@ -14,90 +14,13 @@ from ..dataone_register import \
     D1_lookup, \
     get_package_list
 from ..constants import DataONELocations
+from ..lib.data_map import dataMapDoc
+from ..lib.file_map import fileMapDoc
 
-dataMap = {
-    'type': 'object',
-    'description': ('A container with a basic information about '
-                    'a set of external data resources.'),
-    'properties': {
-        'dataId': {
-            'type': 'string',
-            'description': 'External dataset identificator, such as URL.'
-        },
-        'repository': {
-            'type': 'string',
-            'description': 'Name of a data repository holding the dataset.'
-        },
-        'doi': {
-            'type': 'string',
-            'description': 'Digital Object Identifier'
-        },
-        'name': {
-            'type': 'string',
-            'description': ('A user-friendly name. Defaults to the name '
-                            'provided by an external repository.')
-        },
-        'size': {
-            'type': 'integer',
-            'description': 'Size of the dataset in bytes.'
-        }
-    },
-    'required': ['dataId', 'repository', 'doi', 'name', 'size'],
-    'example': {
-        'dataId': 'urn:uuid:42969280-e11c-41a9-92dc-33964bf785c8',
-        'doi': '10.5063/F1Z899CZ',
-        'name': ('Data from a dynamically downscaled projection of past and '
-                 'future microclimates covering North America from 1980-1999 '
-                 'and 2080-2099'),
-        'repository': 'DataONE',
-        'size': 178679
-    },
-}
 
-fileMap = {
-    'type': 'object',
-    'description': ('A container with a list of filenames and sizes '
-                    'from a DataONE repository.'),
-    'properties': {
-        'name': {
-            'type': 'string',
-            'description': 'The name of the data file.'
-        },
-        'size': {
-            'type': 'integer',
-            'description': 'Size of the file in bytes.'
-        }
-    },
-    'required': ['name', 'fileList'],
-    'example': {
-        "Doctoral Dissertation Research: Mapping Community Exposure to Coastal Climate Hazards"
-        "in the Arctic: A Case Study in Alaska's North Slope":
-            {'fileList':
-                [{'science_metadata.xml':
-                    {'size': 8961}}],
-             'Arctic Slope Shoreline Change Risk Spatial Data Model, 2015-16':
-                 {'fileList':
-                    [{'science_metadata.xml':
-                        {'size': 7577}}]},
-             'North Slope Borough shoreline change risk WebGIS usability workshop.':
-                 {'fileList':
-                    [{'science_metadata.xml':
-                        {'size': 7940}}]},
-             'Local community verification of shoreline change risks along the Alaskan Arctic Ocean'
-                 'coast'
-             ' (North Slope).':
-                 {'fileList':
-                    [{'science_metadata.xml':
-                        {'size': 14250}}]},
-             'Arctic Slope Shoreline Change Susceptibility Spatial Data Model, 2015-16':
-                 {'fileList':
-                    [{'science_metadata.xml':
-                        {'size': 10491}}]}}
-    }
-}
 
-addModel('dataMap', dataMap)
-addModel('fileMap', fileMap)
+addModel('dataMap', dataMapDoc)
+addModel('fileMap', fileMapDoc)
 
 
 def _http_lookup(pid):
