whole-tale/girder_wholetale

Add support for doi and publishedURI field in the Tale. This change spanned the Tale creation code, the Tale schema, as well as the Tale model.

server/models/tale.py
@@ -19,7 +19,7 @@ from girder.exceptions import \
 # Whenever the Tale object schema is modified (e.g. fields are added or
 # removed) increase `_currentTaleFormat` to retroactively apply those
 # changes to existing Tales.
-_currentTaleFormat = 3
+_currentTaleFormat = 4
 
 
 class Tale(AccessControlledModel):
@@ -33,13 +33,14 @@ class Tale(AccessControlledModel):
         })
         self.modifiableFields = {
             'title', 'description', 'public', 'config', 'updated', 'authors',
-            'category', 'icon', 'iframe', 'illustration'
+            'category', 'icon', 'iframe', 'illustration', 'published', 'doi',
+            'publishedURI'
         }
         self.exposeFields(
             level=AccessType.READ,
             fields=({'_id', 'folderId', 'imageId', 'creatorId', 'created',
-                     'format', 'involatileData', 'narrative',
-                     'narrativeId'} | self.modifiableFields))
+                     'format', 'involatileData', 'narrative', 'doi',
+                     'publishedURI', 'narrativeId'} | self.modifiableFields))
         self.exposeFields(level=AccessType.ADMIN, fields={'published'})
 
     def validate(self, tale):
@@ -74,6 +75,12 @@ class Tale(AccessControlledModel):
                 tale['narrativeId'] = default['_id']
             if 'narrative' not in tale:
                 tale['narrative'] = []
+        if tale.get('format', 0) < 4:
+            if 'doi' not in tale:
+                tale['doi'] = None
+            if 'publishedURI' not in tale:
+                tale['publishedURI'] = None
+
         tale['format'] = _currentTaleFormat
         return tale
 
@@ -117,7 +124,7 @@ class Tale(AccessControlledModel):
     def createTale(self, image, data, creator=None, save=True, title=None,
                    description=None, public=None, config=None, published=False,
                    authors=None, icon=None, category=None, illustration=None,
-                   narrative=None):
+                   narrative=None, doi=None, publishedURI=None):
         if creator is None:
             creatorId = None
         else:
@@ -146,7 +153,9 @@ class Tale(AccessControlledModel):
             'title': title,
             'public': public,
             'published': published,
-            'updated': now
+            'updated': now,
+            'doi': doi,
+            'publishedURI': publishedURI
         }
         if public is not None and isinstance(public, bool):
             self.setPublic(tale, public, save=False)

whole-tale/girder_ythub

server/models/tale.py
@@ -33,13 +33,14 @@ class Tale(AccessControlledModel):
         })
         self.modifiableFields = {
             'title', 'description', 'public', 'config', 'updated', 'authors',
-            'category', 'icon', 'iframe', 'illustration', 'dataSet'
+            'category', 'icon', 'iframe', 'illustration', 'dataSet',
+            'published', 'doi', 'publishedURI'
         }
         self.exposeFields(
             level=AccessType.READ,
             fields=({'_id', 'folderId', 'imageId', 'creatorId', 'created',
-                     'format', 'dataSet', 'narrative',
-                     'narrativeId'} | self.modifiableFields))
+                     'format', 'dataSet', 'narrative', 'narrativeId',
+                     'doi', 'publishedURI'} | self.modifiableFields))
         self.exposeFields(level=AccessType.ADMIN, fields={'published'})
 
     def validate(self, tale):
@@ -85,7 +86,8 @@ class Tale(AccessControlledModel):
                 tale['dataSet'].append(
                     {'itemId': obj['_id'], 'mountPath': '/' + obj['name']}
                 )
-
+            tale['doi'] = None
+            tale['publishedURI'] = None
         tale['format'] = _currentTaleFormat
         return tale
 
@@ -129,7 +131,7 @@ class Tale(AccessControlledModel):
     def createTale(self, image, data, creator=None, save=True, title=None,
                    description=None, public=None, config=None, published=False,
                    authors=None, icon=None, category=None, illustration=None,
-                   narrative=None):
+                   narrative=None, doi=None, publishedURI=None):
         if creator is None:
             creatorId = None
         else:
@@ -158,7 +160,9 @@ class Tale(AccessControlledModel):
             'title': title,
             'public': public,
             'published': published,
-            'updated': now
+            'updated': now,
+            'doi': doi,
+            'publishedURI': publishedURI
         }
         if public is not None and isinstance(public, bool):
             self.setPublic(tale, public, save=False)