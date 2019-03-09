2d30f46ed43e589f35d4fcb61a9ac55965e047b0
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2019-03-09 09:29:44 -0600

    Sanitize location of http resources in WT Catalog. Fixes #193

--- server/lib/http_provider.py
+++ server/lib/http_provider.py
@@ -1,9 +1,11 @@
 import os
+import pathlib
 import re
 import requests
 
-from urllib.parse import urlparse
+from urllib.parse import urlparse, unquote
 from girder.utility.model_importer import ModelImporter
+from girder.models.folder import Folder
 
 from .import_providers import ImportProvider
 from .resolvers import DOIResolver
@@ -41,7 +43,7 @@ class HTTPImportProvider(ImportProvider):
             if fname:
                 fname = fname.groups()[0]
         else:
-            fname = os.path.basename(url.path.rstrip('/'))
+            fname = unquote(os.path.basename(url.path.rstrip('/')))
 
         size = headers.get('Content-Length') or \
             headers.get('Content-Range').split('/')[-1]
@@ -59,15 +61,57 @@ class HTTPImportProvider(ImportProvider):
 
     def register(self, parent: object, parentType: str, progress, user, dataMap: DataMap,
                  base_url: str = None):
-        url = dataMap.getDataId()
-        progress.update(increment=1, message='Processing file {}.'.format(url))
+        uri = dataMap.getDataId()
+        url = urlparse(uri)
+        progress.update(increment=1, message='Processing file {}.'.format(uri))
         headers = requests.head(
-            url, headers={'Accept-Encoding': 'identity'}).headers
+            uri, headers={'Accept-Encoding': 'identity'}).headers
         size = headers.get('Content-Length') or \
             headers.get('Content-Range').split('/')[-1]
+
+        parent = Folder().createFolder(
+            parent,
+            url.netloc,
+            description='',
+            parentType=parentType,
+            creator=user,
+            reuseExisting=True,
+        )
+        parentType = 'folder'
+        parent = Folder().setMetadata(
+            parent,
+            {
+                'identifier': '{}://{}'.format(url.scheme, url.netloc),
+                'provider': url.scheme.upper(),
+            }
+        )
+
+        path = pathlib.Path(url.path)
+        for part in path.parts:
+            parent_url = parent['meta']['identifier']
+            new_url = parent_url + '/' + part
+            part = unquote(part)
+            if part in {'/', dataMap.getName()}:
+                continue
+            parent = Folder().createFolder(
+                parent,
+                part,
+                description='',
+                parentType=parentType,
+                creator=user,
+                            reuseExisting=True,
+            )
+            parent = Folder().setMetadata(
+                parent,
+                {
+                    'identifier': new_url,
+                    'provider': url.scheme.upper(),
+                }
+            )
+
         fileModel = ModelImporter.model('file')
         fileDoc = fileModel.createLinkFile(
-            url=url, parent=parent, name=dataMap.getName(), parentType=parentType,
+            url=uri, parent=parent, name=dataMap.getName(), parentType=parentType,
             creator=user, size=int(size),
             mimeType=headers.get('Content-Type', 'application/octet-stream'),
             reuseExisting=True)
@@ -75,6 +119,6 @@ class HTTPImportProvider(ImportProvider):
 
         gc_item = ModelImporter.model('item').load(
             gc_file['itemId'], force=True)
-        gc_item['meta'] = {'identifier': None, 'provider': 'HTTP'}
+        gc_item['meta'] = {'identifier': uri, 'provider': url.scheme.upper()}
         gc_item = ModelImporter.model('item').updateItem(gc_item)
         return ('item', gc_item)
