cd9f4d80139a91ae7c6e1511e22399a4958e1561
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2019-03-25 15:09:22 -0500

    Fix tests

--- plugin_tests/publish_test.py
+++ plugin_tests/publish_test.py
@@ -1,5 +1,5 @@
+from bson import ObjectId
 import mock
-import json
 from tests import base
 
 
@@ -16,54 +16,80 @@ def tearDownModule():
     base.stopServer()
 
 
-class PublishTestCase(base.TestCase):
+class FakeJob:
 
+    job = {}
+
+    def delay(self, *args, **kwargs):
+        return self.job
+
+
+class PublishTestCase(base.TestCase):
     def setUp(self):
         super(PublishTestCase, self).setUp()
-        users = ({
-            'email': 'root@dev.null',
-            'login': 'admin',
-            'firstName': 'Root',
-            'lastName': 'van Klompf',
-            'password': 'secret'
-        }, {
-            'email': 'joe@dev.null',
-            'login': 'joeregular',
-            'firstName': 'Joe',
-            'lastName': 'Regular',
-            'password': 'secret'
-        })
-        self.admin, self.user = [self.model('user').createUser(**user)
-                                 for user in users]
+        users = (
+            {
+                'email': 'root@dev.null',
+                'login': 'admin',
+                'firstName': 'Root',
+                'lastName': 'van Klompf',
+                'password': 'secret',
+            },
+            {
+                'email': 'joe@dev.null',
+                'login': 'joeregular',
+                'firstName': 'Joe',
+                'lastName': 'Regular',
+                'password': 'secret',
+            },
+        )
+        self.admin, self.user = [
+            self.model('user').createUser(**user) for user in users
+        ]
 
-    @mock.patch('gwvolman.tasks.publish')
-    def testPublish(self, it):
-        with mock.patch('girder_worker.task.celery.Task.apply_async', spec=True) \
-                as mock_apply_async:
+        self.tale = self.model('tale', 'wholetale').createTale(
+            {'_id': ObjectId()},
+            data=[],
+            authors=self.user['firstName'] + ' ' + self.user['lastName'],
+            creator=self.user,
+            public=True,
+            description="blah",
+            title="Test",
+            illustration='linkToImage',
+        )
 
-            taleId = 'abc123'
+    def testPublish(self):
+        with mock.patch('gwvolman.tasks.publish.apply_async'), mock.patch(
+            'gwvolman.tasks.publish.delay'
+        ) as dl:
+
+            dl.return_value = FakeJob()
             remoteMemberNode = 'remoteMemberURL'
             authToken = 'tokenXXX'
 
-            mock_apply_async().job.return_value = json.dumps({'job': 1, 'blah': 2})
             resp = self.request(
-                path='/publish/dataone', method='GET', user=self.user,
-                     params={
-                         'taleId': taleId,
-                         'remoteMemberNode': remoteMemberNode,
-                         'authToken': authToken
-                     })
+                path='/publish/dataone',
+                method='GET',
+                user=self.user,
+                params={
+                    'taleId': str(self.tale['_id']),
+                    'remoteMemberNode': remoteMemberNode,
+                    'authToken': authToken,
+                },
+            )
             self.assertStatusOk(resp)
-            job_call = mock_apply_async.call_args_list[-1][-1]
-            self.assertEqual(job_call['headers']['girder_job_title'], 'Publish Tale')
-            self.assertEqual(
-                job_call['kwargs'],
-                ({
-                    'dataone_auth_token': authToken,
-                    'dataone_node': remoteMemberNode,
-                    'tale': taleId,
-                    'user_id': str(self.user['_id'])
-                })
+            job_call = dl.call_args_list[-1][-1]
+            job_call.pop('girder_client_token')
+            self.assertDictEqual(
+                job_call,
+                (
+                    {
+                        'dataone_auth_token': authToken,
+                        'dataone_node': remoteMemberNode,
+                        'tale': str(self.tale['_id']),
+                        'user_id': str(self.user['_id']),
+                    }
+                ),
             )
 
     def tearDown(self):
